064f1f587d1a9f869698b7960866af8bab53c850
Artjom_Stalev <stalev.artjom@gmail.com>
2022-12-14 19:27:17 +0000

    initial commit

---
+++ src/main/java/com/geek/angularity/AngularityApplication.java
@@ -0,0 +1,13 @@
+package com.geek.angularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geek/angularity/AngularityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.angularity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
